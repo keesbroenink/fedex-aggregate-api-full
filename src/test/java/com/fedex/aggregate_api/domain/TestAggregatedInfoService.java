85946d147387b1f2bb6829d45c45e375a704d90b
kees.broenink <kees.broenink@edsnl.nl>
2024-07-15 12:15:02 +0200

    test FedExApi minimal number of request when calling

--- src/test/java/com/fedex/aggregate_api/domain/TestAggregatedInfoService.java
+++ src/test/java/com/fedex/aggregate_api/domain/TestAggregatedInfoService.java
@@ -1,18 +1,22 @@
 package com.fedex.aggregate_api.domain;
 
-import com.fedex.aggregate_api.outbound.FedexApiClient;
 import org.junit.jupiter.api.Test;
-import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
 
 import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class TestAggregatedInfoService {
 
     @Test
     void getInfoNoInput() {
-        FedexApi fedexApi = new FedexApiClient(WebClient.builder(),"http://localhost");
+        FedexApi fedexApi = mock();
         AggregatedInfoService service = new AggregatedInfoService(fedexApi,5);
         AggregatedInfo info = service.getInfo(new AggregatedInfo(emptyList(), emptyList(), emptyList()));
         assertEquals(emptyMap(),info.pricing);
@@ -20,5 +24,26 @@ public class TestAggregatedInfoService {
         assertEquals(emptyMap(),info.shipments);
     }
 
+    @Test
+    void testCacheNotEnoughRequests() {
+        FedexApi fedexApi = mock();
+        AggregatedInfoService service = new AggregatedInfoService(fedexApi,2);
+        AggregatedInfo info = service.getInfo(new AggregatedInfo(List.of("NL"), List.of("1"), List.of("2")));
+        assertEquals(emptyMap(),info.pricing);
+        assertEquals(emptyMap(),info.track);
+        assertEquals(emptyMap(),info.shipments);
+    }
+    @Test
+    void testCacheEnoughRequests() {
+        List<String> orderNumbers = List.of("1");
+        Mono<List<GenericInfo>> answer = Mono.just(List.of(new GenericInfo("1","NEW")));
+        FedexApi fedexApi = mock();
+        given( fedexApi.getTrackStatus(orderNumbers)).willReturn(answer);
+        AggregatedInfoService service = new AggregatedInfoService(fedexApi,1);
+        AggregatedInfo info = service.getInfo(new AggregatedInfo(emptyList(), orderNumbers, emptyList()));
+        assertEquals(emptyMap(),info.pricing);
+        assertEquals(emptyMap(),info.shipments);
+        assertEquals(Map.of("1","NEW"),info.track);
 
+    }
 }
