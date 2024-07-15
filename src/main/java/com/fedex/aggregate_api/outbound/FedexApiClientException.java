36edef676938ea42d7da84f8e946aa2419d2d6c5
kees.broenink <kees.broenink@edsnl.nl>
2024-07-15 12:54:53 +0200

    cleanup

--- src/main/java/com/fedex/aggregate_api/outbound/FedexApiClientException.java
+++ src/main/java/com/fedex/aggregate_api/outbound/FedexApiClientException.java
@@ -1,13 +1,8 @@
 package com.fedex.aggregate_api.outbound;
 
-import org.springframework.http.HttpStatusCode;
-
 public class FedexApiClientException extends RuntimeException{
     public FedexApiClientException(Throwable t) {
         super(t);
     }
 
-    public FedexApiClientException(HttpStatusCode httpStatusCode) {
-        super("Error: "+httpStatusCode.toString());
-    }
 }
