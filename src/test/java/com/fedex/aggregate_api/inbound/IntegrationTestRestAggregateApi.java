a3d30298beac2659c1ee2ee5d566c0736d7c4c7b
kees.broenink <kees.broenink@edsnl.nl>
2024-08-10 10:29:38 +0200

    test improvements

--- src/test/java/com/fedex/aggregate_api/inbound/IntegrationTestRestAggregateApi.java
+++ src/test/java/com/fedex/aggregate_api/inbound/IntegrationTestRestAggregateApi.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockAsyncContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -25,29 +26,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RestAggregateApi.class)
-@Import(AggregatedInfoDeferredService.class)
+@Import({AggregatedInfoDeferredService.class,FedexApiListener.class,AggregatedInfoService.class})
 public class IntegrationTestRestAggregateApi {
     @Autowired
     MockMvc mvc;
 
     @MockBean
-    FedexApiListener fedexApiListener;
+    FedexApi fedexApi;
 
     final ObjectMapper mapper = new ObjectMapper();
 
     @Test
     void testGetAggregatedInfoOnTimeout() throws Exception {
         // client with four requested items will get a response after timeout
-        List<String> orderNumbers = List.of("1","2","3","4");
+        List<String> orderNumbers = List.of("1","2","3","4","5","6","7","8","9","10","11");
         List<TrackingOrderNumber> trackingOrderNumbers = TrackingInfo.fromListString(orderNumbers);
         AggregatedInfo requestResponse = new AggregatedInfo(emptyList(), trackingOrderNumbers, emptyList());
         List<TrackingInfo> tracking = List.of(
                 new TrackingInfo(trackingOrderNumbers.get(0),"NEW"),
                 new TrackingInfo(trackingOrderNumbers.get(1),"NEW"),
-                new TrackingInfo(trackingOrderNumbers.get(2),"NEW"),
-                new TrackingInfo(trackingOrderNumbers.get(3),"NEW"));
+                new TrackingInfo(trackingOrderNumbers.get(3),"NEW"),
+                new TrackingInfo(trackingOrderNumbers.get(4),"NEW"),
+                new TrackingInfo(trackingOrderNumbers.get(5),"NEW"),
+                new TrackingInfo(trackingOrderNumbers.get(6),"NEW"),
+                new TrackingInfo(trackingOrderNumbers.get(7),"NEW"),
+                new TrackingInfo(trackingOrderNumbers.get(8),"NEW"),
+                new TrackingInfo(trackingOrderNumbers.get(9),"NEW"),
+                new TrackingInfo(trackingOrderNumbers.get(10),"NEW"));
         requestResponse.addTracking(tracking);
-        given(fedexApiListener.executeOnTimeout(any())).willReturn(requestResponse);
+        given(fedexApi.getTrackingStatus(any())).willReturn(Mono.just(tracking));
         MvcResult asyncResult = mvc
                 .perform(
                         get( "/aggregation?track="+listToCommaSeparated(orderNumbers))
