53dd596c43d5ccfc8b8a92e7cdc178d62c46a356
kees.broenink <kees.broenink@edsnl.nl>
2024-08-17 11:22:36 +0200

    test improvements

--- src/test/java/com/fedex/aggregate_api/inbound/IntegrationTestRestAggregateApi.java
+++ src/test/java/com/fedex/aggregate_api/inbound/IntegrationTestRestAggregateApi.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 import static com.fedex.aggregate_api.util.StringUtil.listToCommaSeparated;
 import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
@@ -37,6 +38,54 @@ public class IntegrationTestRestAggregateApi {
     final ObjectMapper mapper = new ObjectMapper();
 
     @Test
+    void testGetAggregatedInfoNotEnough() throws Exception {
+        // client with four requested items will get a response after timeout
+        List<String> orderNumbers = List.of("1", "2", "3", "4");
+        // 4 is not enough
+        // client has to wait
+        MvcResult asyncResult = mvc
+                .perform(
+                        get("/aggregation?track=" + listToCommaSeparated(orderNumbers))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        try {
+            mvc.perform(asyncDispatch(asyncResult))
+                    .andExpect(status().isOk());
+        } catch (IllegalStateException e) {
+            return;
+        }
+        fail("Expected Illegal State Exception");
+    }
+    @Test
+    void testGetAggregatedInfo() throws Exception {
+        // client with four requested items will get a response after timeout
+        List<String> orderNumbers = List.of("1","2","3","4");
+        // 4 is not enough
+        // client has to wait
+        MvcResult result = mvc
+                .perform(
+                        get( "/aggregation?track="+listToCommaSeparated(orderNumbers))
+                                .contentType( MediaType.APPLICATION_JSON))
+                .andReturn();
+        // second client will come in and now the call can be done
+        List<String> orderNumbersClient2 = List.of("5");
+        List<TrackingOrderNumber> trackingOrderNumbers = TrackingInfo.fromListString(orderNumbersClient2);
+        AggregatedInfo requestResponse = new AggregatedInfo(emptyList(), trackingOrderNumbers, emptyList());
+        List<TrackingInfo> tracking = List.of(
+                new TrackingInfo(trackingOrderNumbers.get(0),"NEW"));
+        given(fedexApi.getTrackingStatus(any())).willReturn(Mono.just(tracking));
+        requestResponse.addTracking(tracking);
+        MvcResult asyncResult = mvc
+                .perform(
+                        get( "/aggregation?track="+listToCommaSeparated(orderNumbersClient2))
+                                .contentType( MediaType.APPLICATION_JSON))
+                .andReturn();
+        mvc.perform( asyncDispatch( asyncResult))
+                .andExpect( status().isOk())
+                .andExpect( content().string(mapper.writeValueAsString(requestResponse) ));
+    }
+
+    @Test
     void testGetAggregatedInfoOnTimeout() throws Exception {
         // client with four requested items will get a response after timeout
         List<String> orderNumbers = List.of("1","2","3","4","5","6","7","8","9","10","11");
@@ -45,6 +94,7 @@ public class IntegrationTestRestAggregateApi {
         List<TrackingInfo> tracking = List.of(
                 new TrackingInfo(trackingOrderNumbers.get(0),"NEW"),
                 new TrackingInfo(trackingOrderNumbers.get(1),"NEW"),
+                new TrackingInfo(trackingOrderNumbers.get(2),"NEW"),
                 new TrackingInfo(trackingOrderNumbers.get(3),"NEW"),
                 new TrackingInfo(trackingOrderNumbers.get(4),"NEW"),
                 new TrackingInfo(trackingOrderNumbers.get(5),"NEW"),
@@ -69,5 +119,4 @@ public class IntegrationTestRestAggregateApi {
                 .andExpect( status().isOk())
                 .andExpect( content().string(mapper.writeValueAsString(requestResponse) ));
     }
-
 }
